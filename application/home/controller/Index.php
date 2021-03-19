9b29e198ccc0426a62ab0224414c9e95dcf1f3cd
duxinxin <1143122018@qq.com>
2021-03-19 17:15:25 +0800

    ip

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -456,7 +456,7 @@ class Index extends BaseMall
 //        ini_set('memory_limit','3072M');    // 临时设置最大内存占用为3G
         set_time_limit(0);   // 设置脚本最大执行时间 为0 永不过期
 
-        $res = Db::name('ip')->where('id' > 2497)->select();
+        $res = Db::name('ip')->where('id','>',1690)->select();
 
         foreach($res as $key => $val){
 
